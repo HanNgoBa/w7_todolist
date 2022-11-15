49e1ca355321da8ff89235e98750715715dbe07a
HanNgoBa <103867602+HanNgoBa@users.noreply.github.com>
2022-11-15 10:49:16 +0700

    aa

--- src/conponents/Todolist.js
+++ src/conponents/Todolist.js
@@ -2,13 +2,14 @@ import React from "react";
 import Todo from "./Todo";
 
 const Todolist = ({todo,settodo, filterTodo}) =>{
-  
+  console.log(filterTodo.length)
     return(
     <div className="todo-container"> 
       <ul className="todo-list">
-        {filterTodo.length!==0 ? filterTodo.map((dos) => (
+        
+        {filterTodo? filterTodo.map((dos) => (
           <Todo settodo={settodo} text={dos.text} key={dos.id} dos={dos} todo={todo}/>
-        )) : ""}
+        )) : []}
       </ul>
     </div>
     );
